59bad5260b8284bb2a71c1cb18530b5314bc9621
wangjuniorcoder <1519855177@qq.com>
2022-04-03 14:17:52 +0800

    成功进入IA-32e模式

--- srcfile/bootloader/loader.asm
+++ srcfile/bootloader/loader.asm
@@ -25,6 +25,7 @@ Loop_DetMem:
     ;finish detecting
     mov si, DetectMsg
     call Func_Print
+
 Load_Protect_Mode:
     ; open address A20
     push ax
@@ -42,7 +43,6 @@ Load_Protect_Mode:
     or eax, 1
     mov cr0, eax
     jmp dword code32_selector:Protect_Mode
-
 Func_Print:
     push ax
     mov ah, 0xE
@@ -56,7 +56,6 @@ Func_Print:
     .return:
         pop ax
         ret
-
 LoadMsg: db "Begin to load", 10, 13, 0
 DetectMsg: db "Detecting Memory completed", 10, 13, 0
 Error:
@@ -64,23 +63,59 @@ Error:
     call Func_Print
     hlt
     .errmsg db "Error : Failed to load", 10, 13, 0
-
 [bits 32]
 Protect_Mode:
-    mov ax, data32_selector
+    mov ax, 0x10
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
     mov ss, ax
     mov esp, 0x10000; change the stack top
-
+    ; get kernel
     mov edi, 0x100000;target memory address
     mov ecx, 10;tenth sector
     mov bl, 200;amount of sectors
     call Func_Read
-    xchg bx, bx
-    jmp dword code32_selector:0x100000
+    call Func_Check_Sup
+    test eax, eax
+    jz Not_Sup
+    mov dword [0x90000], 0x91007
+    mov dword [0x90800], 0x91007
+    mov dword [0x91000], 0x92007
+    mov dword [0x92000], 0x000083
+    mov dword [0x92008], 0x200083
+    mov dword [0x92010], 0x400083
+    mov dword [0x92018], 0x600083
+    mov dword [0x92020], 0x800083
+    mov dword [0x92028], 0xa00083
+    db 0x66
+    lgdt [gdt64_ptr]
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov esp, 0x10000; change the stack top
+    ; open PAE(physical address extension)
+    mov eax, cr4
+    bts eax, 5
+    mov cr4, eax
+    ; load cr3
+    mov eax, 0x90000
+    mov cr3, eax
+    ; enable long mode
+    mov ecx, 0C0000080h
+    rdmsr
+    bts eax, 8
+    wrmsr
+    ;open PE and paging
+    mov eax, cr0
+    bts eax, 0
+    bts eax, 31
+    mov cr0, eax
+    jmp dword code64_selector:0x100000
 ; LBA Mode
 Func_Read:
     push ecx
@@ -105,15 +140,15 @@ Func_Read:
     mov al, cl
     out dx, al
     ; 0x1F6
-    inc dx
-    shr ecx, 8
-    mov al, 0b1110_0000
-    and cl, 0b0000_1111
-    or al, cl
-    out dx, al
+    inc dx 
+    shr ecx, 8 
+    mov al, 0b1110_0000 
+    and cl, 0b0000_1111 
+    or al, cl 
+    out dx, al 
     ; 0x1F7(read data from disk)
-    inc dx
-    mov al, 0x20
+    inc dx 
+    mov al, 0x20 
     out dx, al 
 
     xor ecx, ecx
@@ -147,6 +182,23 @@ Func_Read:
     pop dx
     pop ecx
     ret
+Func_Check_Sup:
+    mov eax, 0x80000000
+    cpuid
+    cmp eax, 0x80000001
+    setnb al
+    jb Func_Check_Done
+    mov eax, 0x80000001
+    cpuid
+    bt edx, 29
+    setc al
+Func_Check_Done:
+    movzx eax, al
+    ret
+Not_Sup:
+    mov byte[0xb8000], 'N'
+    jmp $
+
 
 code32_selector equ (1 << 3)
 data32_selector equ (2 << 3)
@@ -172,6 +224,17 @@ gdt32_data: dd 0x0000FFFF, 0x00C09200
     ; db 0b_1_1_0_0_0000 | (memory_base >> 16) & 0xF; unit=4k, 32 bit, not 64bit, memory limit(16-19 bit)
     ; db (memory_base >> 24) & 0xFF; base address, 24-31 bit  
 gdt32_end:
+
+code64_selector equ gdt64_code - gdt64_base
+data64_selector equ gdt64_data - gdt64_base
+gdt64_ptr:
+    dw (gdt64_end - gdt64_base) - 1
+    dd gdt64_base
+gdt64_base:
+    dq 0; NULL discriptor, 8 bytes.
+gdt64_code: dq 0x00209A0000000000
+gdt64_data: dq 0x0020920000000000
+gdt64_end:
 
 ards_count:
     dw 0
